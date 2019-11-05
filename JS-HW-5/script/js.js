4685436de8317f4900210c839d7ddb68d073b278
KananValizada <kenan.veli@yahoo.com>
2019-11-06 00:16:24 +0400

    age method improved

--- JS-HW-5/script/js.js
+++ JS-HW-5/script/js.js
@@ -6,7 +6,7 @@ let newUser
 function createNewUser(){
 firstname =prompt("enter your name");
 lastname = prompt("enter your surname");
-birthday = prompt("enter your birthday","dd.mm.yy")
+birthday = prompt("enter your birthday","dd.mm.yyyy")
 newUser = { 
     firstname,
     lastname,
@@ -15,8 +15,21 @@ newUser = {
     },
     getAge:function(){
         let now =new Date()
-        let year =now.getFullYear()
-        return year - parseInt(birthday.split(".")[2])
+        let year =now.getFullYear();
+        let month=now.getMonth() +1
+        let day =now.getDate()
+        if(birthday.split(".")[1]<month ){
+            return year - parseInt(birthday.split(".")[2])
+        }else if(birthday.split(".")[1]==month){
+            if(birthday.split(".")[0]<=day){
+                return year - parseInt(birthday.split(".")[2])
+            }else {
+                return year - parseInt(birthday.split(".")[2]) -1
+            }
+        }else{
+            return year - parseInt(birthday.split(".")[2]) -1
+        }
+        
     },
     getPassword:function(){
         return firstname[0].toUpperCase()+lastname.toLowerCase()+birthday.split(".")[2]
