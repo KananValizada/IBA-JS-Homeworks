7c36a86d6f291d996d5ff5e844cfa3fd1d6d492a
Kanan R. Valizada <KananRv@CODE.LOCAL>
2019-11-11 20:25:37 +0400

    JS-10 added

---
+++ JS-HW-10/js/script.js
@@ -0,0 +1,30 @@
+let icon =document.querySelectorAll(".fas")
+let password =document.querySelectorAll(".password")
+let btn = document.getElementById("btn")
+let span =document.createElement("span")
+span.innerHTML="You need to enter the identical values"
+span.style.cssText="color:red"
+for(let i=0;i<icon.length; i++){
+    icon[i].addEventListener("click",()=>{
+            icon[i].classList.toggle("fa-eye-slash")
+            if(icon[i].className.includes("fa-eye-slash")){
+                password[i].type="password"
+            }else{
+                password[i].type="text"
+            }
+    })
+    password[i].addEventListener("input",display)
+}
+function display(){
+    span.style.display="none"
+}
+
+btn.addEventListener("click",compare)
+function compare() {
+if(password[0].value==password[1].value){
+    alert("You are welcome")
+}else {
+    span.style.display="block"
+btn.before(span)
+}
+}
