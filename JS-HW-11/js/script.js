70b9113f898fa7d160647c685b1ead33706ab4bf
Kanan R. Valizada <KananRv@CODE.LOCAL>
2019-11-11 20:52:09 +0400

    JS 11 added

---
+++ JS-HW-11/js/script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelectorAll(".btn") 
+ document.addEventListener("keydown",colorChange)
+
+    function colorChange(event){
+        btn.forEach((x)=>{
+            x.classList.remove("blue")
+
+        })
+        for(let i=0;i<btn.length;i++){
+            if(event.key==btn[i].innerHTML.toLowerCase()){
+                btn[i].classList.toggle("blue")
+            }else if(event.key=="Enter"&&btn[i].innerHTML=="Enter"){
+                btn[i].classList.toggle("blue")
+            }
+        }
+        console.log(event.key)
+    }
+    
