6169b00e9532265b3a71dd180dd0f215119b2f5a
KananValizada <kenan.veli@yahoo.com>
2019-11-21 23:52:29 +0400

    HW 11 Bug Fixed

--- JS-HW-11/js/script.js
+++ JS-HW-11/js/script.js
@@ -1,18 +1,20 @@
-let btn = document.querySelectorAll(".btn") 
- document.addEventListener("keydown",colorChange)
-
-    function colorChange(event){
-        btn.forEach((x)=>{
-            x.classList.remove("blue")
-
-        })
-        for(let i=0;i<btn.length;i++){
-            if(event.key==btn[i].innerHTML.toLowerCase()){
-                btn[i].classList.toggle("blue")
-            }else if(event.key=="Enter"&&btn[i].innerHTML=="Enter"){
-                btn[i].classList.toggle("blue")
-            }
+let btn = document.querySelectorAll(".btn")
+document.addEventListener("keydown", () => {
+    btn.forEach(x => {
+        if (x.innerHTML.toLowerCase() == event.key || event.key == "Enter") {
+            btn.forEach((x) => {
+                x.classList.remove("blue")
+            })
         }
-        console.log(event.key)
     }
-    
+    )
+    for (let i = 0; i < btn.length; i++) {
+        if (event.key == btn[i].innerHTML.toLowerCase()) {
+            btn[i].classList.toggle("blue")
+        } else if (event.key == "Enter" && btn[i].innerHTML == "Enter") {
+            btn[i].classList.toggle("blue")
+        }
+    }
+})
+
+
